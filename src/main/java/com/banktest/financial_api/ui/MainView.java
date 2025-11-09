e32f239806c7a6f66c5183f1581c9a21e45cd8f4
conradoovictor <conrado.victor01@gmail.com>
2025-11-09 10:22:36 -0300

    Implementando vaadin no projeto

---
+++ src/main/java/com/banktest/financial_api/ui/MainView.java
@@ -0,0 +1,13 @@
+package com.banktest.financial_api.ui;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("") 
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new H1("VAADIN IMPLEMENTADO!"));
+    }
+}
